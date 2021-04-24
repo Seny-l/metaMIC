8a28054850e1d532388489dff3fe91dcc01e5bba
Seny-l <81548043+Seny-l@users.noreply.github.com>
2021-04-25 00:46:37 +0800

    Update install_model.sh

--- metaMIC/model/install_model.sh
+++ metaMIC/model/install_model.sh
@@ -1,7 +1,7 @@
 # download IDBA_UD/MEGAHIT-specific training models for metaMIC
 
-wget https://zenodo.org/record/4717667/files/IDBA_UD.tar.gz
-wget https://zenodo.org/record/4717667/files/MEGAHIT.tar.gz
+wget https://zenodo.org/record/4717743/files/IDBA_UD.tar.gz
+wget https://zenodo.org/record/4717743/files/MEGAHIT.tar.gz
 tar xzvf IDBA_UD.tar.gz
 tar xzvf MEGAHIT.tar.gz
 gunzip IDBA_UD/*
