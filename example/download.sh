70f549543e014dfbade710e7e28042956948acfe
Seny-l <81548043+Seny-l@users.noreply.github.com>
2021-04-25 00:45:59 +0800

    Update download.sh

--- example/download.sh
+++ example/download.sh
@@ -1,6 +1,7 @@
 # download example data
 
-wget https://zenodo.org/record/4717725/files/insert_600_1__cov100x.fastq.gz
-wget https://zenodo.org/record/4717725/files/insert_600_2__cov100x.fastq.gz
-wget https://zenodo.org/record/4717725/files/R.sphaeroides.bam.gz
+wget https://zenodo.org/record/4717743/files/insert_600_1__cov100x.fastq.gz
+wget https://zenodo.org/record/4717743/files/insert_600_2__cov100x.fastq.gz
+wget https://zenodo.org/record/4717743/files/R.sphaeroides.bam.gz
+wget https://zenodo.org/record/4717743/files/R.sphaeroides_pileup.out
 tar xzvf *.gz
