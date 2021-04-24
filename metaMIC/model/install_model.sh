8e34e87cb4e4bdfdd57055b9f070b1e7abdb564e
Seny-l <81548043+Seny-l@users.noreply.github.com>
2021-04-24 23:00:53 +0800

    Create install_model.sh

---
+++ metaMIC/model/install_model.sh
@@ -0,0 +1,8 @@
+# download IDBA_UD/MEGAHIT-specific training models for metaMIC
+
+wget https://zenodo.org/record/4717667/files/IDBA_UD.tar.gz
+wget https://zenodo.org/record/4717667/files/MEGAHIT.tar.gz
+tar xzvf IDBA_UD.tar.gz
+tar xzvf MEGAHIT.tar.gz
+gunzip IDBA_UD/*
+gunzip MEGAHIT/*
